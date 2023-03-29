7839353d32eba8b1b7f4a519a090d338a723c588
thelonehegelian <thelonehegelian@gmail.com>
2023-03-29 10:40:16 +0100

    Add types and import in server/index

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import express, { Express } from 'express';
+import boydParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+const helmet = require('helmet');
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
