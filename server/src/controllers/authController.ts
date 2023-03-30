58f06d80d9881e859fc7410b1240224f83a68322
thelonehegelian <thelonehegelian@gmail.com>
2023-03-30 10:42:07 +0100

    Add register function

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,5 +1,26 @@
 // bcrypt is a library for hashing passwords
 import bcrypt from 'bcrypt';
+import { Request, Response } from 'express';
 // jwt is a library for generating and verifying JWT tokens
 // what is special about JWT tokens is that they are signed with a secret key and can be verified by the server without storing any session data
 import jwt from 'jsonwebtoken';
+
+// register a user
+
+export const register = async (req: Request, res: Response) => {
+  // reques is coming from the client
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+    } = req.body; // req.body is destructured to get the data from the user input
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server error');
+  }
+};
