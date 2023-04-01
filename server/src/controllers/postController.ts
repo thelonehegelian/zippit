0e65564433269f83953404e5e07729e1da8bb56a
thelonehegelian <thelonehegelian@gmail.com>
2023-04-01 09:57:43 +0100

    Add function signatures

---
+++ server/src/controllers/postController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import { Post } from '../models/post.model';
+
+export const createPost = async (req: Request, res: Response) => {};
+export const getFeedPosts = async (req: Request, res: Response) => {};
+export const getUserPosts = async (req: Request, res: Response) => {};
+export const likePost = async (req: Request, res: Response) => {};
+
+// export default createPost;
